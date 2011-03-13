ff4ac734e92dc384350b0e2b22b15bd1353acd88
Kris Khaira <kris.khaira@gmail.com>
2011-03-13 17:49:12 +0800

    Excludes for TAR.

--- backup-latest.sh
+++ backup-latest.sh
@@ -3,4 +3,4 @@ rm ~/backup/latest-3.tar.gz
 mv ~/backup/latest-2.tar.gz ~/backup/latest-3.tar.gz
 mv ~/backup/latest-1.tar.gz ~/backup/latest-2.tar.gz
 mv ~/backup/latest.tar.gz ~/backup/latest-1.tar.gz
-tar czvf ~/backup/latest.tar.gz ~/public_html/sites/default/files
+tar czvf --exclude-from=~/devkit/backup-exclude.txt ~/backup/latest.tar.gz ~/public_html/sites/default/files
