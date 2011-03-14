74ee95dca166a6ae9c7d2c945429900a863b77ed
Kris Khaira <kris.khaira@gmail.com>
2011-03-14 15:06:22 +0800

    Updated backup frequency.

--- backup-latest.sh
+++ backup-latest.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-rm ~/backup/latest-3.tar.gz
-mv ~/backup/latest-2.tar.gz ~/backup/latest-3.tar.gz
+
+# Renumber the existing backups
+rm ~/backup/latest-2.tar.gz
 mv ~/backup/latest-1.tar.gz ~/backup/latest-2.tar.gz
 mv ~/backup/latest.tar.gz ~/backup/latest-1.tar.gz
-tar czvf --exclude-from=~/devkit/backup-exclude.txt ~/backup/latest.tar.gz ~/public_html/sites/default/files
+
+# Make a latest backup of the public folders
+tar czvf --exclude-from=~/devkit/backup-exclude.txt ~/backup/latest.tar.gz ~/public*
