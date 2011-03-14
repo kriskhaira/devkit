2ed2acb8b469c361c7ce01ea72b4283c0e12f07c
Kris Khaira <kris.khaira@gmail.com>
2011-03-14 15:10:56 +0800

    Put quotes around the exclude paths.

--- backup-archive.sh
+++ backup-archive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rsync --delete --exclude-from=~/devkit/backup-exclude.txt -apP /home ~/backup/archive/
-rsync --delete --exclude-from=~/devkit/backup-exclude.txt -apP /etc/apache2/sites-available ~/backup/archive/
-rsync --delete --exclude-from=~/devkit/backup-exclude.txt -apP /etc/nginx/sites-available ~/backup/archive/
-rsync --delete --exclude-from=~/devkit/backup-exclude.txt -apP /etc/php5 ~/backup/archive/
-rsync --delete --exclude-from=~/devkit/backup-exclude.txt -apP /etc/mysql ~/backup/archive/
-rsync --delete --exclude-from=~/devkit/backup-exclude.txt -apP /root/backup/ex* ~/backup/archive/
+rsync --delete --exclude-from="~/devkit/backup-exclude.txt" -apP /home ~/backup/archive/
+rsync --delete --exclude-from="~/devkit/backup-exclude.txt" -apP /etc/apache2/sites-available ~/backup/archive/
+rsync --delete --exclude-from="~/devkit/backup-exclude.txt" -apP /etc/nginx/sites-available ~/backup/archive/
+rsync --delete --exclude-from="~/devkit/backup-exclude.txt" -apP /etc/php5 ~/backup/archive/
+rsync --delete --exclude-from="~/devkit/backup-exclude.txt" -apP /etc/mysql ~/backup/archive/
+rsync --delete --exclude-from="~/devkit/backup-exclude.txt" -apP /root/backup/ex* ~/backup/archive/
