42563e49a720d150f1e6107a50237e50039ba3c6
Kris Khaira <kris.khaira@gmail.com>
2011-04-01 15:16:40 +0800

    Better example for latest backup script.

--- backup-latest.sh
+++ backup-latest.sh
@@ -5,8 +5,9 @@ rm -rf ~/backup/latest-2
 mv ~/backup/latest-1 ~/backup/latest-2
 cp -r ~/backup/latest ~/backup/latest-1
 
+# Optional MySQL dumps
+mysqldump -u USER -p PASS DATABASE > DATABASE.sql
+
 # Make a latest backup of the public_html folders or any other folder starting with public_
 rsync -apP --delete ~/public_* ~/backup/latest/
-
-# Or you can do this if you already have some files in CVS and only want to back up some upload folders
-# rsync -apP --delete ~/public_html/sites/default/files ~/backup/latest/SITENAME-files
+rsync -apP --delete ~/*.sql ~/backup/latest/
